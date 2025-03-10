e068335cccd9b31a5e9bcfffeab3a4dae192c516
aWhiteScreen <nemura.zygimantasa@gmail.com>
2025-03-10 12:22:49 +0200

    lab4 and some comments for lab3

--- src/test/java/Lab3Test.java
+++ src/test/java/Lab3Test.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -26,23 +27,30 @@ public class Lab3Test {
 
     private static FirefoxDriver driver;
     //private static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    private static String email = "bolsonarista3064@gmail.com";
+    private static Random rand = new Random();
+    private static String email = String.format("bolsonarista%d@gmail.com", rand.nextInt(1000));
     private static String password = "jair4eva";
 
     @BeforeClass // Creates a user before any test
     public static void UserCreation() {
+        // 1. Open the website: https://demowebshop.tricentis.com/
         driver = new FirefoxDriver();
         driver.get("https://demowebshop.tricentis.com/");
         driver.manage().window().maximize();
+        // 2. Click "Log in"
         driver.findElement(By.linkText("Log in")).click();
+        // 3. Click "Register" in the "New Customer" section
         driver.findElement(By.className("register-button")).click();
+        // 4. Fill in the registration form fields
         driver.findElement(By.id("gender-male")).click();
         driver.findElement(By.id("FirstName")).sendKeys("Fernando");
         driver.findElement(By.id("LastName")).sendKeys("Laprovitola");
         driver.findElement(By.id("Email")).sendKeys(email);
         driver.findElement(By.id("Password")).sendKeys(password);
         driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+        // 5. Click "Register"
         driver.findElement(By.id("register-button")).click();
+        // 6. Click "Continue"
         driver.findElement(By.className("register-continue-button")).click();
         driver.quit();
     }
@@ -51,6 +59,7 @@ public class Lab3Test {
     public void setUp() {
       driver = new FirefoxDriver();
       driver.manage().window().maximize();
+      // 1. Open the website: https://demowebshop.tricentis.com/
       driver.get("https://demowebshop.tricentis.com/");
 
     }
@@ -62,23 +71,27 @@ public class Lab3Test {
 
     public void testScenario(String dataFile) throws IOException {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //Login
+        // 2. Click "Log in"
         driver.findElement(By.linkText("Log in")).click();
+        // 3. Fill in the "Email" and "Password" fields and click "Log in"
         driver.findElement(By.id("Email")).sendKeys(email);
         driver.findElement(By.id("Password")).sendKeys(password);
         driver.findElement(By.className("login-button")).click();
-        //wait.until(ExpectedConditions.elementToBeClickable(By.linkText("DigitalDo")))
+        // 4. In the sidebar menu, select "Digital downloads"
         driver.findElement(By.linkText("Digital downloads")).click();
 
+        // 5. Add products to the cart by reading from a text file:
         Path filePath = Paths.get(dataFile);
-        List<String> items = Files.readAllLines(filePath);
+        List<String> items = Files.readAllLines(filePath); // Reads Items from list
         for (String item : items) {
             WebElement digitalDownload = driver.findElement(By.linkText(item));
             WebElement itemBox = digitalDownload.findElement(By.xpath("./ancestor::div[contains(@class, 'item-box')]"));
             itemBox.findElement(By.className("product-box-add-to-cart-button")).click();
         }
 
+        // 6. Open "Shopping cart"
         driver.findElement(By.className("cart-label")).click();
+        // 7. Check "I agree" and click "Checkout"
         WebElement acceptTerms = driver.findElement(By.id("termsofservice"));
         wait.until(ExpectedConditions.elementToBeClickable(acceptTerms));
         acceptTerms.click();
@@ -86,6 +99,7 @@ public class Lab3Test {
         wait.until(ExpectedConditions.elementToBeClickable(checkoutButton));
         checkoutButton.click();
 
+        // 8. In "Billing Address", select an existing address or fill in a new one, then click "Continue"
         List<WebElement> buttons = driver.findElements(By.id("billing-address-select"));
 
         if (!buttons.isEmpty()) {
@@ -103,30 +117,34 @@ public class Lab3Test {
             driver.findElement(By.className("new-address-next-step-button")).click();
         }
 
+        // 9. In "Payment Method", click "Continue"
         WebElement paymentMethod = driver.findElement(By.className("payment-method-next-step-button"));
         wait.until(ExpectedConditions.elementToBeClickable(paymentMethod));
         paymentMethod.click();
 
+        // 10. In "Payment Information", click "Continue"
         WebElement paymentInfo = driver.findElement(By.className("payment-info-next-step-button"));
         wait.until(ExpectedConditions.elementToBeClickable(paymentInfo));
         paymentInfo.click();
 
+        // 11. In "Confirm Order", click "Confirm"
         WebElement confirmOrder = driver.findElement(By.className("confirm-order-next-step-button"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView(true);", confirmOrder);
         confirmOrder.click();
 
+        // 12. Ensure the order is successfully placed.
         wait.until(ExpectedConditions.elementToBeClickable(By.className("order-completed-continue-button")));
         String result = driver.findElement(By.className("title")).getText();
         assertEquals(result, "Your order has been successfully processed!");
 
     }
 
-    @Test
+    @Test // o Test 1: Read from data1.txt
     public void testCaseOne() throws IOException {
         testScenario("test1.txt");
     }
-    @Test
+    @Test // o Test 2: Read from data2.txt
     public void testCaseTwo() throws IOException {
         testScenario("test2.txt");
     }
---
+++ src/test/java/Lab4Test.java
@@ -0,0 +1,94 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.time.Duration;
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Lab4Test { // 4) change password, confirm new password works and old one does not.
+
+    private static FirefoxDriver driver;
+    //private static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    private static Random rand = new Random();
+    private static String email = String.format("bolsonarista%d@gmail.com", rand.nextInt(1000));
+    private static String password = "jair4eva";
+    private static String newPassword = "lula4eva";
+
+    @BeforeClass // Creates a user before any test
+    public static void UserCreation() {
+        driver = new FirefoxDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Log in")).click();
+        driver.findElement(By.className("register-button")).click();
+        driver.findElement(By.id("gender-male")).click();
+        driver.findElement(By.id("FirstName")).sendKeys("Fernando");
+        driver.findElement(By.id("LastName")).sendKeys("Laprovitola");
+        driver.findElement(By.id("Email")).sendKeys(email);
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.id("ConfirmPassword")).sendKeys(password);
+        driver.findElement(By.id("register-button")).click();
+        driver.findElement(By.className("register-continue-button")).click();
+        driver.findElement(By.className("account")).click();
+        driver.findElement(By.linkText("Change password")).click();
+        driver.findElement(By.id("OldPassword")).sendKeys(password);
+        driver.findElement(By.id("NewPassword")).sendKeys(newPassword);
+        driver.findElement(By.id("ConfirmNewPassword")).sendKeys(newPassword);
+        driver.findElement(By.className("change-password-button")).click();
+
+
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() {
+      driver = new FirefoxDriver();
+      driver.manage().window().maximize();
+      driver.get("https://demowebshop.tricentis.com/");
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    } 
+
+    @Test // Test with the new password
+    public void newPasswordTest() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.findElement(By.linkText("Log in")).click();
+        driver.findElement(By.id("Email")).sendKeys(email);
+        driver.findElement(By.id("Password")).sendKeys(newPassword);
+        driver.findElement(By.className("login-button")).click();
+
+        WebElement accountButton = driver.findElement(By.linkText(email));
+        wait.until(ExpectedConditions.elementToBeClickable(accountButton));
+        assertTrue(accountButton.isDisplayed());
+    }
+
+    @Test // Test with the old password
+    public void oldPasswordTest() {
+        driver.findElement(By.linkText("Log in")).click();
+        driver.findElement(By.id("Email")).sendKeys(email);
+        driver.findElement(By.id("Password")).sendKeys(password);
+        driver.findElement(By.className("login-button")).click();
+
+        String result = driver.findElement(By.xpath("//div[@class='validation-summary-errors']//li")).getText();
+        assertEquals("The credentials provided are incorrect", result);
+        
+    }
+
+
+
+}
